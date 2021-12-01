ca99167bc4b9b8cb2a9cf1c62bf037805233bc1f
jan-martinek <honza.martinek@gmail.com>
2021-12-01 21:17:19 +0000

    Deploying to docs from @ books-are-next/gellner-po-nas-at-prijde-potopa@311bf87954b8d20843c113b3ad9c89f77f9fc3a3 🚀

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-7575cbf';
+const CACHE = 'cache-311bf87';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -40,7 +40,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./favicon.png","./colophon.html","./index.html","./manifest.json","./section0003.html","./section0004.html","./section0005.html","./section0006.html","./section0007.html","./section0008.html","./section0009.html","./section0010.html","./section0011.html","./section0012.html","./section0013.html","./section0014.html","./section0015.html","./section0016.html","./section0017.html","./section0018.html","./section0019.html","./section0020.html","./section0021.html","./section0022.html","./section0023.html","./section0024.html","./section0025.html","./section0026.html","./section0027.html","./section0028.html","./section0029.html","./section0030.html","./section0031.html","./section0032.html","./section0033.html","./section0034.html","./section0035.html","./section0036.html","./section0037.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/88x31.png","./resources/obalka.jpg","./resources/pd-88x31.png","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./template-images/circles.png","./style/style.min.css"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./section0003.html","./section0004.html","./section0005.html","./section0006.html","./section0007.html","./section0008.html","./section0009.html","./section0010.html","./section0011.html","./section0012.html","./section0013.html","./section0014.html","./section0015.html","./section0016.html","./section0017.html","./section0018.html","./section0019.html","./section0020.html","./section0021.html","./section0022.html","./section0023.html","./section0024.html","./section0025.html","./section0026.html","./section0027.html","./section0028.html","./section0029.html","./section0030.html","./section0031.html","./section0032.html","./section0033.html","./section0034.html","./section0035.html","./section0036.html","./section0037.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/88x31.png","./resources/obalka.jpg","./resources/pd-88x31.png","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./template-images/circles.png","./style/style.min.css"]));
 }
 
 self.addEventListener('fetch', e => {
